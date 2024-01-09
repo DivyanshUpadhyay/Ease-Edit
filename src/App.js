2e6be72703312ee5b06b3e1645da96624fa9b57d
Divyansh Upadhyay <divyanshupadhyay2003@gmail.com>
2024-01-09 11:41:09 +0530

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+//import About from "./components/About";
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Textform from "./components/Textform";
+import Alert from "./components/Alert";
 function App() {
+  const [mode, setMode]= useState('light');
+  const [alert, setAlert]= useState(null);
+  const showAlert=(message , type)=>
+  {
+    setAlert(
+      {
+        msg : message,
+        type : type
+      }
+    )
+    setTimeout(()=>
+    {
+      setAlert(null);
+    }
+    ,1000)
+  }
+
+  const toggleMode = () => 
+  {
+    if(mode === 'light')
+    {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode is Enabled","success");
+    }
+    else
+    {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Normal mode is Enabled","success");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <>
+<Navbar title="Edit Ease" mode={mode} toggleMode={toggleMode}/>
+<Alert alert={alert}/>
+    <div className="container">
+    <Textform showAlert= {showAlert}heading="Enter the Text" mode={mode}></Textform>
     </div>
+    </>
   );
 }
 
